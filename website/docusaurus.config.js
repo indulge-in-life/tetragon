e789e40855ce7bb11bdb90bc4e02a8eeb2fae57f
indulge-in-life <pravinkumar.kathirmani@gmail.com>
2026-09-09 21:00:52 +0200

    Update docusaurus.config.js

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -47,7 +47,7 @@ const config = {
       ({
         docs: {
           sidebarPath: './sidebars.js',
-          routeBasePath: '/',
+          routeBasePath: '/docs/overview',
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
